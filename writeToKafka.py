500e01854ce83df48ca5ecab659f847842c30522
vinayak-sanketika <165653132+vinayak-sanketika@users.noreply.github.com>
2024-04-27 00:07:19 +0530

    First commit

---
+++ writeToKafka.py
@@ -0,0 +1,42 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, concat_ws
+
+
+
+def write_to_kafka(csv_path, topic_name, bootstrap_servers):
+    
+
+    spark = SparkSession.builder \
+        .appName("CSV to Kafka") \
+        .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1")\
+        .getOrCreate()
+
+    # Read CSV data as a DataFrame
+    df = spark.read \
+        .format("csv") \
+        .option("header", True)  \
+        .option("delimiter", ",")  \
+        .load(csv_path)
+
+   
+        # Writing each row as a separate string 
+        
+    string_df = df.select(concat_ws(",", col("StudentID"), col("CGPA"),col("Internships"), col("Projects"),col("Certifications"), col("AptitudeTestScore"),col("SoftSkillsRating"),col("ExtracurricularActivities"), col("PlacementTraining"),col("SSC_Marks"),col("HSC_Marks"),col("PlacementStatus")).alias("value"))
+
+
+    string_df.write \
+            .format("kafka") \
+            .option("topic", topic_name) \
+            .option("kafka.bootstrap.servers", bootstrap_servers) \
+            .save()
+
+    spark.stop()
+
+
+if __name__ == "__main__":
+    csv_path = "/opt/spark/spark-app/spark_df/placement.csv"  
+    topic_name = "topic1" 
+    bootstrap_servers = "localhost:9092"  
+
+
+    write_to_kafka(csv_path, topic_name, bootstrap_servers)
